82c582970a2c04e56bfa781e9fd7ee12394c7026
Jacob Archambault <jacobarchambault@gmail.com>
2025-10-31 22:52:38 -0400

    Update sample code to test ingestion example.

--- archambault-jacob_w/src/EFPOC.Console/Program.cs
+++ archambault-jacob_w/src/EFPOC.Console/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EdiFabric.Core.Model.Edi;
+using EdiFabric.Framework.Readers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x12File = File.OpenRead(@"../../samples/HIPAA/ClaimPayment.txt");
+            EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996"); 
+
+            List<IEdiItem> ediItems;
+            using (var reader = new X12Reader(x12File, "EdiFabric.Templates.X12",
+            new X12ReaderSettings {ContinueOnError = true }))
+            {
+                ediItems = reader.ReadToEnd().ToList();
+            }
+        }
+    }
+}
