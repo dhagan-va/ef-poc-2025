e0f504f9ad954576de66f1a20d8d3ada0831349a
Jacob Archambault <jacobarchambault@gmail.com>
2025-11-03 19:37:58 -0500

    Minimal example printing out property-value pairs to console using reflection

--- archambault-jacob_w/src/EFPOC.Console/Program.cs
+++ archambault-jacob_w/src/EFPOC.Console/Program.cs
@@ -1,8 +1,10 @@
 ﻿using EdiFabric.Core.Model.Edi;
 using EdiFabric.Framework.Readers;
+using EdiFabric.Templates.Hipaa5010;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace ConsoleApp
 {
@@ -10,14 +12,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var x12File = File.OpenRead(@"../../samples/HIPAA/ClaimPayment.txt");
             EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996"); 
 
             List<IEdiItem> ediItems;
-            using (var reader = new X12Reader(x12File, "EdiFabric.Templates.X12",
+            using (var reader = new X12Reader(File.OpenRead(@"../../samples/HIPAA/ClaimPayment.txt"), "EdiFabric.Templates.Hipaa",
             new X12ReaderSettings {ContinueOnError = true }))
             {
                 ediItems = reader.ReadToEnd().ToList();
+            }
+            foreach (var ediItem in ediItems)
+            {
+                PropertyInfo[] objectInfo = ediItem.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (var propInfo in objectInfo)
+                    Console.WriteLine($"{propInfo.Name}: \t{propInfo.GetValue(ediItem, null)}");
+
+                Console.WriteLine();
             }
         }
     }
