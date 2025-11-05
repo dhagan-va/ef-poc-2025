64342e1a15429e5143a0576256cf202180bcb87d
Jacob Archambault <jacobarchambault@gmail.com>
2025-11-04 21:08:05 -0500

    Move some code into file IO project

--- archambault-jacob_w/src/EFPOC.Console/Program.cs
+++ archambault-jacob_w/src/EFPOC.Console/Program.cs
@@ -1,9 +1,7 @@
 ﻿using EdiFabric.Core.Model.Edi;
 using EdiFabric.Framework.Readers;
 using EdiFabric.Templates.Hipaa5010;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using EFPOC.FileIO;
 using System.Reflection;
 using System.Text.Json;
 
@@ -13,16 +11,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996"); 
-
-            List<IEdiItem> ediItems;
-            using (var reader = new X12Reader(File.OpenRead(@"../../samples/HIPAA/ClaimPayment.txt"), "EdiFabric.Templates.Hipaa",
-            new X12ReaderSettings {ContinueOnError = true }))
-            {
-                ediItems = reader.ReadToEnd().ToList();
-            }
-            var claims = ediItems.OfType<TS837P>();
-            Console.WriteLine($"Number of Claims: {ediItems.Count}");
+            EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996");
+            var claims = new TS837PTxtReader().Read();
             Console.WriteLine("Console version using reflection (Doesn't unpack all properties yet):");
             foreach (var ediItem in claims)
             {
