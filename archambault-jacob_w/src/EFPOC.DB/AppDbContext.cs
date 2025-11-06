9b776911a39f75facffc880d5a36a4663aa83777
Jacob Archambault <jacobarchambault@gmail.com>
2025-11-05 20:04:15 -0500

    Add DbContext

---
+++ archambault-jacob_w/src/EFPOC.DB/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EdiFabric.Core.Model.Edi;
+using EdiFabric.Templates.Hipaa5010;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFPOC.DB;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public AppDbContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HIPAA;User Id=sa;Password=Foobar123!;TrustServerCertificate=True");
+        }
+    }
+
+    public DbSet<TS837P> TS837s => Set<TS837P>();
+}
