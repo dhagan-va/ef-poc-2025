5e47befd7942fd8776a9ef2184479519c88003cf
CarlosLadino <carlosladinocl@yahoo.com>
2025-11-06 14:26:42 -0500

    Initial Setup

---
+++ carlos-l/EDI837.Ingestion/EDI837/src/Models/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EDI837.src.Models
+{
+    public class AppDataContext: DbContext
+    {
+        public AppDataContext(DbContextOptions options) : base(options) 
+        { 
+        
+        }
+       
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
