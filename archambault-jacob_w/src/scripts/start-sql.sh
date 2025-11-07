e5ae21d21e0562d975f991488e4fd56236645cfb
Jacob Archambault <jacobarchambault@gmail.com>
2025-11-07 15:37:47 -0500

    Add docker compose

--- archambault-jacob_w/src/scripts/start-sql.sh
+++ archambault-jacob_w/src/scripts/start-sql.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --env-file ../../.env -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:latest
+docker run --env-file ../.env -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:latest
