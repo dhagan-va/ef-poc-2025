d25f1fa78e30661d317c4a3f61cf639f7c142128
JacobArchambault <39110529+JacobArchambault@users.noreply.github.com>
2025-11-17 14:27:13 +0000

    Minor change

--- archambault-jacob_w/src/EFPOC.ConsoleApp/Program.cs
+++ archambault-jacob_w/src/EFPOC.ConsoleApp/Program.cs
@@ -11,9 +11,8 @@ namespace EFPOC.ConsoleApp
         {
             EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996");
             var claims = new TS837PTxtReader().Read();
-            var parser = new TS837Parser();
             Console.WriteLine("Console version using reflection (Doesn't unpack all properties yet):");
-            parser.Parse(claims);
+            new TS837Parser().Parse(claims);
             Console.WriteLine("JSON Version: ");
             Console.WriteLine(JsonSerializer.Serialize(claims, new JsonSerializerOptions { WriteIndented = true }));
         }
