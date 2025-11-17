8ee401e9d4dacb3bbc8e386f9b9c45ec4bb21ed0
Eric Vogel <Evogel@symssg.com>
2025-11-17 16:44:04 -0500

    initial setup using docker

---
+++ eric-vogel_e/src/Edi837Ingester/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996");
+
+Console.WriteLine("Hello, World!");
---
+++ eric-vogel_e/src/Edi837Ingester/Services/EdiParser.cs
@@ -0,0 +1,6 @@
+namespace Edi837Ingester.Services;
+
+public class EdiParser
+{
+    
+}
