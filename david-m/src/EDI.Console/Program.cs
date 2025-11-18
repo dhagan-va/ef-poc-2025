5d23e32a41cf2b66268cb021d40cd835ef35cda2
David-Morris- <dmorriscz7e@gmail.com>
2025-11-18 08:13:17 -0500

    Add vw_EdiFileIngestionDetails view with comprehensive transaction details
    - Includes claim amount, patient control number, provider details
    - Joins CLM, NM1, REF tables for 837P/I/D transactions
    - Includes CLP data for 835 transactions
    - Provides detailed view for retrieving all aspects of a TransactionRecordId
    - Union query across all EDI transaction types for complete coverage

--- david-m/src/EDI.Console/Program.cs
+++ david-m/src/EDI.Console/Program.cs
@@ -94,6 +94,119 @@ using (var scope = host.Services.CreateScope())
         LEFT JOIN DTM_ProductionDate AS dtm ON dtm.Id = t.DTM_ProductionDateId;
         """);
 
+    // Create detailed transaction view with claim information
+    dbContext.Database.ExecuteSqlRaw(
+        """
+        CREATE OR ALTER VIEW dbo.vw_EdiFileIngestionDetails AS
+        SELECT
+            t.Id AS TransactionRecordId,
+            '837P' AS TransactionSet,
+            st.TransactionSetIdentifierCode_01 AS TransactionSetIdentifierCode,
+            st.TransactionSetControlNumber_02 AS TransactionSetControlNumber,
+            bht.SubmitterTransactionIdentifier_03 AS TransactionReferenceNumber,
+            bht.TransactionSetCreationDate_04 AS TransactionSetCreationDate,
+            bht.TransactionSetCreationTime_05 AS TransactionSetCreationTime,
+            bht.TransactionSetPurposeCode_02 AS TransactionSetPurposeCode,
+            bht.TransactionTypeCode_06 AS TransactionTypeCode,
+            clm.PatientControlNumber_01 AS PatientControlNumber,
+            clm.TotalClaimChargeAmount_02 AS TotalClaimChargeAmount,
+            clm.ClaimFilingIndicatorCode_05 AS ClaimFilingIndicatorCode,
+            il.NameLastOrOrganizationName_03 AS PatientLastName,
+            il.NameFirst_04 AS PatientFirstName,
+            pr.EntityIdentifierCode_01 AS ProviderEntityCode,
+            pr.NameLastOrOrganizationName_03 AS ProviderName,
+            npi.IdentificationCode_04 AS ProviderNPI
+        FROM T837PTransactions AS t
+        LEFT JOIN ST AS st ON st.Id = t.STId
+        LEFT JOIN BHT_BeginningOfHierarchicalTransaction_8 AS bht ON bht.Id = t.BHT_BeginningOfHierarchicalTransactionId
+        LEFT JOIN CLM_CleanClaimBenefitDataIdentification AS clm ON clm.Id = t.CLM_CleanClaimBenefitDataIdentificationId
+        LEFT JOIN NM1_InformationReceiverName_1 AS il ON il.Id = t.NM1_InformationReceiverName_1Id
+        LEFT JOIN NM1_BillingProviderName AS pr ON pr.Id = t.NM1_BillingProviderNameId
+        LEFT JOIN REF_BillingProviderSecondaryIdentifier AS npi ON npi.Id = t.REF_BillingProviderSecondaryIdentifierId
+
+        UNION ALL
+
+        SELECT
+            t.Id AS TransactionRecordId,
+            '837I' AS TransactionSet,
+            st.TransactionSetIdentifierCode_01 AS TransactionSetIdentifierCode,
+            st.TransactionSetControlNumber_02 AS TransactionSetControlNumber,
+            bht.SubmitterTransactionIdentifier_03 AS TransactionReferenceNumber,
+            bht.TransactionSetCreationDate_04 AS TransactionSetCreationDate,
+            bht.TransactionSetCreationTime_05 AS TransactionSetCreationTime,
+            bht.TransactionSetPurposeCode_02 AS TransactionSetPurposeCode,
+            bht.TransactionTypeCode_06 AS TransactionTypeCode,
+            clm.PatientControlNumber_01 AS PatientControlNumber,
+            clm.TotalClaimChargeAmount_02 AS TotalClaimChargeAmount,
+            clm.ClaimFilingIndicatorCode_05 AS ClaimFilingIndicatorCode,
+            il.NameLastOrOrganizationName_03 AS PatientLastName,
+            il.NameFirst_04 AS PatientFirstName,
+            pr.EntityIdentifierCode_01 AS ProviderEntityCode,
+            pr.NameLastOrOrganizationName_03 AS ProviderName,
+            npi.IdentificationCode_04 AS ProviderNPI
+        FROM T837ITransactions AS t
+        LEFT JOIN ST AS st ON st.Id = t.STId
+        LEFT JOIN BHT_BeginningOfHierarchicalTransaction_8 AS bht ON bht.Id = t.BHT_BeginningOfHierarchicalTransactionId
+        LEFT JOIN CLM_CleanClaimBenefitDataIdentification AS clm ON clm.Id = t.CLM_CleanClaimBenefitDataIdentificationId
+        LEFT JOIN NM1_InformationReceiverName_1 AS il ON il.Id = t.NM1_InformationReceiverName_1Id
+        LEFT JOIN NM1_BillingProviderName AS pr ON pr.Id = t.NM1_BillingProviderNameId
+        LEFT JOIN REF_BillingProviderSecondaryIdentifier AS npi ON npi.Id = t.REF_BillingProviderSecondaryIdentifierId
+
+        UNION ALL
+
+        SELECT
+            t.Id AS TransactionRecordId,
+            '837D' AS TransactionSet,
+            st.TransactionSetIdentifierCode_01 AS TransactionSetIdentifierCode,
+            st.TransactionSetControlNumber_02 AS TransactionSetControlNumber,
+            bht.SubmitterTransactionIdentifier_03 AS TransactionReferenceNumber,
+            bht.TransactionSetCreationDate_04 AS TransactionSetCreationDate,
+            bht.TransactionSetCreationTime_05 AS TransactionSetCreationTime,
+            bht.TransactionSetPurposeCode_02 AS TransactionSetPurposeCode,
+            bht.TransactionTypeCode_06 AS TransactionTypeCode,
+            clm.PatientControlNumber_01 AS PatientControlNumber,
+            clm.TotalClaimChargeAmount_02 AS TotalClaimChargeAmount,
+            clm.ClaimFilingIndicatorCode_05 AS ClaimFilingIndicatorCode,
+            il.NameLastOrOrganizationName_03 AS PatientLastName,
+            il.NameFirst_04 AS PatientFirstName,
+            pr.EntityIdentifierCode_01 AS ProviderEntityCode,
+            pr.NameLastOrOrganizationName_03 AS ProviderName,
+            npi.IdentificationCode_04 AS ProviderNPI
+        FROM T837DTransactions AS t
+        LEFT JOIN ST AS st ON st.Id = t.STId
+        LEFT JOIN BHT_BeginningOfHierarchicalTransaction_8 AS bht ON bht.Id = t.BHT_BeginningOfHierarchicalTransactionId
+        LEFT JOIN CLM_CleanClaimBenefitDataIdentification AS clm ON clm.Id = t.CLM_CleanClaimBenefitDataIdentificationId
+        LEFT JOIN NM1_InformationReceiverName_1 AS il ON il.Id = t.NM1_InformationReceiverName_1Id
+        LEFT JOIN NM1_BillingProviderName AS pr ON pr.Id = t.NM1_BillingProviderNameId
+        LEFT JOIN REF_BillingProviderSecondaryIdentifier AS npi ON npi.Id = t.REF_BillingProviderSecondaryIdentifierId
+
+        UNION ALL
+
+        SELECT
+            t.Id AS TransactionRecordId,
+            '835' AS TransactionSet,
+            st.TransactionSetIdentifierCode_01 AS TransactionSetIdentifierCode,
+            st.TransactionSetControlNumber_02 AS TransactionSetControlNumber,
+            trn.CurrentTransactionTraceNumber_02 AS TransactionReferenceNumber,
+            dtm.Date_02 AS TransactionSetCreationDate,
+            dtm.Time_03 AS TransactionSetCreationTime,
+            NULL AS TransactionSetPurposeCode,
+            NULL AS TransactionTypeCode,
+            clp.PatientControlNumber_03 AS PatientControlNumber,
+            clp.ClaimSubmissionAmount_04 AS TotalClaimChargeAmount,
+            NULL AS ClaimFilingIndicatorCode,
+            NULL AS PatientLastName,
+            NULL AS PatientFirstName,
+            NULL AS ProviderEntityCode,
+            NULL AS ProviderName,
+            NULL AS ProviderNPI
+        FROM T835Transactions AS t
+        LEFT JOIN ST AS st ON st.Id = t.STId
+        LEFT JOIN TRN_DependentTraceNumber AS trn ON trn.Id = t.TRN_ReassociationTraceNumberId
+        LEFT JOIN DTM_ProductionDate AS dtm ON dtm.Id = t.DTM_ProductionDateId
+        LEFT JOIN CLP_ClaimPaymentInformation AS clp ON clp.Id = t.CLP_ClaimPaymentInformationId;
+        """);
+
     EdiFabric.SerialKey.Set("c417cb9dd9d54297a55c032a74c87996");
 
     // Set environment variable for AWS service URL (moto server)
