3e6e600c99742ecbb2b637e7e2a4b9449ee3dcc1
evogelsymssg <Evogel@symssg.com>
2025-11-21 12:43:52 -0500

    clean up

--- eric-vogel_e/src/Edi837Ingester/Services/EdiParserService.cs
+++ eric-vogel_e/src/Edi837Ingester/Services/EdiParserService.cs
@@ -1,4 +1,5 @@
 using Edi837Ingester.Data.Repositories;
+using EdiFabric.Core.Model.Edi;
 using EdiFabric.Core.Model.Edi.ErrorContexts;
 using EdiFabric.Framework.Readers;
 using EdiFabric.Templates.Hipaa5010;
@@ -25,53 +26,60 @@ public class EdiParserService(IEdiRepository ediRepository, ILogger<EdiParserSer
         using var reader = new X12Reader(ediStream, _ => typeof(TS837P).Assembly);
 
         var ediItems = (await reader.ReadToEndAsync()).ToList();
+
+        // Separate by type
+        if (!ediItems.Any())
+        {
+            logger.LogWarning("No EDI transactions found in file {FilePath}", filePath);
+            return;
+        }
+
         var professionalItems = ediItems.OfType<TS837P>();
         var institutionalItems = ediItems.OfType<TS837I>();
         var dentalItems = ediItems.OfType<TS837D>();
 
-        if (professionalItems.Any())
-        {
-            // check for errors
-            var errors = professionalItems.Where(x => x.ErrorContext != null && x.ErrorContext.HasErrors);
-            foreach (var error in errors)
-            {
-                logger.LogError("Error parsing Professional claim: {Errors}", string.Join(", ",
-                    error.ErrorContext.Errors.Select(e => e.Message)));
-            }
+        var claimType = professionalItems.Any() ? "Professional" :
+            institutionalItems.Any() ? "Institutional" :
+            dentalItems.Any() ? "Dental" : "Unknown";
 
-            logger.LogInformation("Found {Count} Professional claims", professionalItems.Count());
-            professionalClaims.AddRange(professionalItems);
-            await ediRepository.Save(professionalClaims);
+        switch(claimType)
+        {
+            case "Professional":
+                ReportErrors(professionalItems, claimType);
+                LogCount(professionalItems, claimType);
+                professionalClaims.AddRange(professionalItems);
+                await ediRepository.Save(professionalClaims);
+                break;
+            case "Institutional":
+                ReportErrors(institutionalItems, claimType);
+                LogCount(institutionalItems, claimType);
+                institutionalClaims.AddRange(institutionalItems);
+                await ediRepository.Save(institutionalClaims);
+                break;
+            case "Dental":
+                ReportErrors(dentalItems, claimType);
+                LogCount(dentalItems, claimType);
+                dentalClaims.AddRange(dentalItems);
+                await ediRepository.Save(dentalClaims);
+                break;
+            case "Unknown":
+                logger.LogWarning("No recognized 837 claim transactions found in file {FilePath}", filePath);
+                return;
         }
+    }
 
-        if (institutionalItems.Any())
+    private void LogCount(IEnumerable<EdiMessage> items, string claimType)
+    {
+        logger.LogInformation("Found {Count} {ClaimType} claims", items.Count(), claimType);
+    }
+
+    private void ReportErrors(IEnumerable<EdiMessage> items, string claimType)
+    {
+        var errors = items.Where(x => x.ErrorContext != null && x.ErrorContext.HasErrors);
+        foreach (var error in errors)
         {
-            // check for errors
-            var errors = institutionalItems.Where(x => x.ErrorContext != null && x.ErrorContext.HasErrors);
-            foreach (var error in errors)
-            {
-                logger.LogError("Error parsing Institutional claim: {Errors}", string.Join(", ",
-                    error.ErrorContext.Errors.Select(e => e.Message)));
-            }
-
-            logger.LogInformation("Found {Count} Institutional claims", institutionalItems.Count());
-            institutionalClaims.AddRange(institutionalItems);
-            await ediRepository.Save(institutionalClaims);
-        }
-
-        if (dentalItems.Any())
-        {
-            // check for errors
-            var errors = dentalItems.Where(x => x.ErrorContext != null && x.ErrorContext.HasErrors);
-            foreach (var error in errors)
-            {
-                logger.LogError("Error parsing Dental claim: {Errors}", string.Join(", ",
-                    error.ErrorContext.Errors.Select(e => e.Message)));
-            }
-
-            logger.LogInformation("Found {Count} Dental claims", dentalItems.Count());
-            dentalClaims.AddRange(dentalItems);
-            await ediRepository.Save(dentalClaims);
+            logger.LogError("Error parsing {ClaimType} claim: {Errors}", claimType, string.Join(", ",
+                error.ErrorContext.Errors.Select(e => e.Message)));
         }
     }
 }
